c2a721e51f028976acbf1d8049751bb4fdbf3c17
JonBckr <125697426+beggga@users.noreply.github.com>
2025-10-16 14:25:25 +0200

    Update file_test.py

--- file_test.py
+++ file_test.py
@@ -1,23 +1,24 @@
 
-import pytest
-
-
 def test_calc_addition():
     output = 2 + 4
     assert output == 6
 
 
 def test_calc_substraction():
+    # Testet das Ergebnis von 2 - 4
     output = 2 - 4
     assert output == -2
 
 
 def test_calc_multiply():
+    # Testet das Ergebnis von 2 * 4
     output = 2 * 4
     assert output == 8
 
 
 def test_coucou():
+    # Testet, ob der Output 'hello' ist
     output = 'hello'
     assert output == 'hello'
+
 
